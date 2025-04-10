13dbb41445ded2e49bc8b82947cf87bb9327a471
Ramesh Kumar Sao <selusao@gmail.com>
2025-04-10 11:20:49 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,7 +173,7 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+//var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
